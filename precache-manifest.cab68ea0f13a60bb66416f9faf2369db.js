c2bcc81d58772eab88a4b713881f82c4615453a9
shinw97 <shinw97@gmail.com>
2019-10-13 13:55:29 +0800

    Updates

---
+++ precache-manifest.cab68ea0f13a60bb66416f9faf2369db.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc98a8e7397e2f945ec876e0cfcd3ee",
+    "url": "/buddy-graduation-card-2019/index.html"
+  },
+  {
+    "revision": "1b19a3bb69b66ec6a208",
+    "url": "/buddy-graduation-card-2019/static/css/main.dee031f2.chunk.css"
+  },
+  {
+    "revision": "2dbc183595892cf4f106",
+    "url": "/buddy-graduation-card-2019/static/js/2.4eac6797.chunk.js"
+  },
+  {
+    "revision": "1b19a3bb69b66ec6a208",
+    "url": "/buddy-graduation-card-2019/static/js/main.4c55ca52.chunk.js"
+  },
+  {
+    "revision": "1dc0bca11cad7f1c3497",
+    "url": "/buddy-graduation-card-2019/static/js/runtime-main.f50a8e33.js"
+  },
+  {
+    "revision": "3b4b355107214d96bcd309c53ccc044b",
+    "url": "/buddy-graduation-card-2019/static/media/first-page.3b4b3551.PNG"
+  },
+  {
+    "revision": "2fe9f4c14f194b9521b0b032246d6139",
+    "url": "/buddy-graduation-card-2019/static/media/ganjie.2fe9f4c1.jpg"
+  },
+  {
+    "revision": "b2f87f6fe864affbf95b362cebb1f393",
+    "url": "/buddy-graduation-card-2019/static/media/grad-hat-vector.b2f87f6f.png"
+  },
+  {
+    "revision": "9db79ca85318b65e2510905e9ad5f189",
+    "url": "/buddy-graduation-card-2019/static/media/graduation-comic-doodle-time-1254473.9db79ca8.jpeg"
+  },
+  {
+    "revision": "d22d7ac022c944d6773ac494d9581c40",
+    "url": "/buddy-graduation-card-2019/static/media/kamwoh.d22d7ac0.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/buddy-graduation-card-2019/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e60c6edaa7dd73dad592a97f146fc9d8",
+    "url": "/buddy-graduation-card-2019/static/media/otter-grad.e60c6eda.jpg"
+  },
+  {
+    "revision": "162c12899ae596797034513b0b22d0e0",
+    "url": "/buddy-graduation-card-2019/static/media/plane1.162c1289.png"
+  },
+  {
+    "revision": "4a7c1fa3b4b6baea41d300644a024afb",
+    "url": "/buddy-graduation-card-2019/static/media/red-green-blue.4a7c1fa3.png"
+  },
+  {
+    "revision": "e9399c95c4aaebddbc47de453ffe85d7",
+    "url": "/buddy-graduation-card-2019/static/media/signatures.e9399c95.png"
+  },
+  {
+    "revision": "f66261837bbdc00caa28ceef20081e8f",
+    "url": "/buddy-graduation-card-2019/static/media/test-wishes.f6626183.PNG"
+  },
+  {
+    "revision": "6450a383b328dd774c646a7f217e70fd",
+    "url": "/buddy-graduation-card-2019/static/media/wishes-gj.6450a383.PNG"
+  },
+  {
+    "revision": "8700435d880c7ceb97fa5f008e6fafb5",
+    "url": "/buddy-graduation-card-2019/static/media/wishes-woh.8700435d.PNG"
+  },
+  {
+    "revision": "7075c653e663e24a9582008229f8ff53",
+    "url": "/buddy-graduation-card-2019/static/media/wishes-yk.7075c653.PNG"
+  },
+  {
+    "revision": "7a6075c1c75e7db7a1f3c8d2671c1be1",
+    "url": "/buddy-graduation-card-2019/static/media/yukang.7a6075c1.jpg"
+  }
+]);
